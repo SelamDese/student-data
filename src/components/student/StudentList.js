da4d7e39f7a1c59325f5eaf69a027ac79f549b9b
selam <selam0315@gmail.com>
2019-01-28 10:06:04 -0600

    show students first and last names

---
+++ src/components/student/StudentList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class StudentList extends Component {
+  render() {
+    return (
+      <section className="students">
+        {this.props.students.map(student => (
+          <div key={student.id}>
+               <p> {student.firstName} {student.lastName}</p>
+               {/* <p> {student.lastName}</p> */}
+          </div>
+        ))}
+      </section>
+    );
+  }
+}
